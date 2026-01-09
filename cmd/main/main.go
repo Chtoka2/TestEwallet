ffb9d115e63f7d61099458520bf8a04a0b43f0e1
Chtoka <roman1232medvedev@yandex.ru>
2026-01-09 19:27:45 +0300

    Maked init to storage

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,20 +4,27 @@ import (
 	"e-wallet/config"
 	"e-wallet/internal/lib/env"
 	"e-wallet/internal/logger"
+	"e-wallet/internal/storage"
+	"os"
 )
 
 func main(){
 	//TODO: init config
 	cfg := config.Config_init()
 	//TODO: init .env
-	env := env.Env_reader()
-	
+	dbPath := env.Env_reader()
 	//TODO: init logger
 	log := logger.Logger_init(cfg.Env)
 	log.Info("Start new logger!")
 	
 	//TODO: init storage
-	
+	db, err := storage.New(dbPath)
+	if err != nil{
+		log.Error("Some problem")
+		os.Exit(1)
+	}
+	_ = db
+	log.Info("Storage was init")
 	//TODO: init router
 
 	//TODO: init server
--- internal/lib/env/env.go
+++ internal/lib/env/env.go
@@ -11,9 +11,25 @@ func Env_reader() string{
 	if err := godotenv.Load(); err != nil{
 		log.Fatal("File .env not exists")	
 	}
-	dbUrl := os.Getenv("DATABASE_URL")
-	if dbUrl == ""{
-		log.Fatal("dbUrl is empty")
+	host := os.Getenv("host")
+	if host == ""{
+		log.Fatal("Host is empty")
 	}
-	return dbUrl
+	user := os.Getenv("user")
+	if user == ""{
+		log.Fatal("User is empty")
+	}
+	dbname := os.Getenv("dbname")
+	if dbname == ""{
+		log.Fatal("Dbname is empty")
+	}
+	password := os.Getenv("password")
+	if password == ""{
+		log.Fatal("Password is empty")
+	}
+	sslmode := os.Getenv("sslmode")
+	if sslmode == ""{
+		sslmode="disable"
+	}
+	return "host="+host+" user="+user+" dbname="+dbname+" password="+password+" sslmode="+sslmode
 }
